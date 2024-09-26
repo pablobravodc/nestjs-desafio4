fbdee4cb5f982519f183281802b5fa24e2d3d951
pablobravodc <105530593+pablobravodc@users.noreply.github.com>
2024-09-25 23:46:41 -0400

    Primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RepuestosModule } from './repuestos/repuestos.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { UserGuard } from './user/user.guard';
+import { RolesGuard } from './user/roles/roles.guard';
+import { ConfigModule } from '@nestjs/config'
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot('mongodb://localhost:27017/repuesto', { 
+      MongooseModule.forRoot(`${process.env.MONGODB_URL}`),
+    RepuestosModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_GUARD',
+      useClass: UserGuard,
+    },
+    {
+      provide: 'APP_GUARD',
+      useClass: RolesGuard,
+    },
+  ],
+})
+export class AppModule {}
+
